2992986799fc531ade3c69c1cdddd04b7fb36626
Mac-ziis <macg286@gmail.com>
2023-10-07 13:01:46 -0700

    Add Views

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -8,71 +8,71 @@ namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
-    // private readonly ToDoListContext _db;
+    private readonly HairSalonContext _db;
 
-    // public ItemsController(ToDoListContext db)
-    // {
-    //   _db = db;
-    // }
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
 
-    // public ActionResult Index()
-    // {
-    //   List<Item> model = _db.Items
-    //                       .Include(item => item.Category)
-    //                       .ToList();
-    //   return View(model);
-    // }
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients
+                          .Include(client => client.Stylist)
+                          .ToList();
+      return View(model);
+    }
 
-    // public ActionResult Create()
-    // {
-    //   return View();
-    // }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
-    // [HttpPost]
-    // public ActionResult Create(Item item)
-    // {
-    //   if (item.CategoryId == 0)
-    //   {
-    //     return RedirectToAction("Create");
-    //   }
-    //   _db.Items.Add(item);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      if (client.StylistId == 0)
+      {
+        return RedirectToAction("Create");
+      }
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Details(int id)
-    // {
-    //   Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   return View(thisItem);
-    // }
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
 
-    // public ActionResult Edit(int id)
-    // {
-    //   Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   return View(thisItem);
-    // }
+    public ActionResult Edit(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Item item)
-    // {
-    //   _db.Items.Update(item);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Clients.Update(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult Delete(int id)
-    // {
-    //   Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   return View(thisItem);
-    // }
+    public ActionResult Delete(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
 
-    // [HttpPost, ActionName("Delete")]
-    // public ActionResult DeleteConfirmed(int id)
-    // {
-    //   Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
-    //   _db.Items.Remove(thisItem);
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,8 +4,8 @@ namespace HairSalon.Models
 {
     public class Stylist
     {
-        // public int StylistId { get; set; }
-        // public string Name { get; set; }
-        // public List<Client> Clients { get; set; }
+        public int StylistId { get; set; }
+        public string Name { get; set; }
+        public List<Client> Clients { get; set; }
     }
 }
